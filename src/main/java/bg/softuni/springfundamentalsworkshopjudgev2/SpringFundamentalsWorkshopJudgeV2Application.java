62fff40f703d5c57baa47dd8194a8bc97e0f0fbb
dimit <dimitar_rradev@abv.bg>
2025-01-13 11:53:21 +0200

    first commit

---
+++ src/main/java/bg/softuni/springfundamentalsworkshopjudgev2/SpringFundamentalsWorkshopJudgeV2Application.java
@@ -0,0 +1,13 @@
+package bg.softuni.springfundamentalsworkshopjudgev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsWorkshopJudgeV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalsWorkshopJudgeV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/springfundamentalsworkshopjudgev2/SpringFundamentalsWorkshopJudgeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.springfundamentalsworkshopjudgev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundamentalsWorkshopJudgeV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
